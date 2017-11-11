72489695a7ba7901e8ffeac286a33e160079d0e2
Francesco Balestrieri <bale@balenet.com>
2017-11-11 22:27:54 +0200

    First import

---
+++ Assets/DetectorController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectorController : MonoBehaviour {
+
+	public DialController m_dial;
+	float m_min = 0.4f;
+	float m_max = 5;
+	float m_intensity = 0.5f;
+	float m_maxBeepInterval = 3f;
+	float m_minBeepInterval = 0.5f;
+	float m_elapsedTime = 0;
+	float m_beepInterval;
+
+	AudioSource m_player;
+
+	public void SetScale(float min, float max) {
+		m_min = min;
+		m_max = max;
+	}
+
+	public void SetValue(float v) {
+		v = Mathf.Abs(v);
+		if (v > m_max) {
+			m_intensity = 1;
+		} else {
+			m_intensity = (v - m_min) / (m_max - m_min);
+		}
+		m_dial.SetIntensity(m_intensity);
+		m_beepInterval = m_minBeepInterval + (m_maxBeepInterval - m_minBeepInterval) * m_intensity;
+	}
+
+	// Use this for initialization
+	void Start () {
+		m_player = GetComponentsInChildren<AudioSource>()[0];
+		m_minBeepInterval = m_player.clip.length;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.activeInHierarchy) {
+			m_elapsedTime += Time.deltaTime;
+			if (m_elapsedTime >= m_beepInterval) {
+					m_player.Play();
+					m_elapsedTime = 0;
+			}
+		}
+	}
+}
---
+++ Assets/GameLogic.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEngine.XR.iOS {
+	public class GameLogic : MonoBehaviour {
+
+		public DetectorController m_detector;
+		public GameObject m_marker;
+		public GameObject m_camera;
+		public Text m_distanceText;
+		public Text m_debugText;
+
+		// Use this for initialization
+		void Start () {
+			m_detector.SetScale(0.2f,5);
+		}
+
+		void OnEnable()
+    {
+        UnityARSessionNativeInterface.ARSessionTrackingChangedEvent += TrackingChanged;
+    }
+
+		void TrackingChanged(UnityARCamera camera) {
+			Debug.Log("Tracking changed");
+
+			if (camera.trackingState == ARTrackingState.ARTrackingStateNormal) {
+				m_debugText.text = "State: Normal (" + camera.trackingState.ToString() + ")";
+			}
+			if (camera.trackingState == ARTrackingState.ARTrackingStateLimited) {
+				m_debugText.text = "State: Limited (" + camera.trackingState.ToString() + ")";
+			}
+			// m_debugText.text = camera.trackingState.ToString() + ", " + camera.trackingReason.ToString();
+		}
+		// Update is called once per frame
+		void Update () {
+			float distance = Vector3.Distance(m_marker.transform.position, m_camera.transform.position);
+			m_detector.SetValue(distance);
+			m_distanceText.text = distance.ToString("N");
+		}
+	}
+}
