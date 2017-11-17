f8503afafbfc8faf7646128780d9bde6aa1fd1bf
Francesco Balestrieri <bale@balenet.com>
2017-11-17 16:15:45 +0200

    Refactor: move assets to separate subfolders

---
+++ Assets/Scripts/DialController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialController : MonoBehaviour {
+
+	float min_rotation = -80;
+	float max_rotation = 80;
+	float m_rotation = -80;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+
+	public void SetIntensity(float value) {
+		m_rotation = min_rotation + value * (max_rotation - min_rotation);
+	}
+
+	void Update () {
+    	transform.eulerAngles = new Vector3(0, 0, m_rotation);
+	}
+}
---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEngine.XR.iOS {
+	public class GameLogic : MonoBehaviour {
+
+		public DetectorController m_detector;
+		public GameObject m_marker;
+		public GameObject m_camera;
+		public GameObject m_MainMenuPanel;
+		public GameObject m_ScanningPanel;
+		public GameObject m_PlaceMarkerPanel;
+		public GameObject m_SettingsPanel;
+		public Dropdown m_AreaDropdown;
+
+		public Text m_distanceText;
+		public Text m_debugText;
+
+		enum State {Start, Scanning, Placing, Searching};
+
+		private State m_state = State.Start;
+		private ARTrackingState m_tracking_state = ARTrackingState.ARTrackingStateNotAvailable;
+
+		const string GAME_AREA_MAX_PREF = "GameAreaSizeMax";
+
+		float[] m_maxScale = {5f, 15f, 50f};
+
+		// Use this for initialization
+		void Start () {
+			int scaleOption = PlayerPrefs.GetInt(GAME_AREA_MAX_PREF);
+			m_AreaDropdown.value = scaleOption;
+			m_detector.SetScale(0.2f, m_maxScale[scaleOption]);
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		}
+
+		void OnEnable()
+    {
+        UnityARSessionNativeInterface.ARSessionTrackingChangedEvent += TrackingChanged;
+    }
+
+		void TrackingChanged(UnityARCamera camera) {
+			Debug.Log("Tracking changed");
+			m_tracking_state = camera.trackingState;
+
+			if (camera.trackingState == ARTrackingState.ARTrackingStateNormal) {
+				m_debugText.text = "State: Normal (" + camera.trackingState.ToString() + ")";
+				if (m_state == State.Scanning) {
+					m_ScanningPanel.SetActive(false);
+					m_PlaceMarkerPanel.SetActive(true);
+				}
+			}
+			if (camera.trackingState == ARTrackingState.ARTrackingStateLimited) {
+				m_debugText.text = "State: Limited (" + camera.trackingState.ToString() + ")";
+			}
+			// m_debugText.text = camera.trackingState.ToString() + ", " + camera.trackingReason.ToString();
+		}
+
+		public void OnStartButton() {
+			if (m_tracking_state == ARTrackingState.ARTrackingStateNormal) {
+				m_PlaceMarkerPanel.SetActive(true);
+				m_state = State.Placing;
+			} else {
+				m_ScanningPanel.SetActive(true);
+				m_state = State.Scanning;
+			}
+			m_MainMenuPanel.SetActive(false);
+		}
+
+		public void OnSettingsButton() {
+			m_MainMenuPanel.SetActive(false);
+			m_SettingsPanel.SetActive(true);
+		}
+
+		public void SetGameAreaSize(int s) {
+			Debug.Log("Max scale option value = " + s.ToString());
+			m_detector.SetMaxScale(m_maxScale[s]);
+			PlayerPrefs.SetInt(GAME_AREA_MAX_PREF, s);
+			Debug.Log("Max scale = " + m_maxScale[s].ToString());
+		}
+
+		// Update is called once per frame
+		void Update () {
+			float distance = Vector3.Distance(m_marker.transform.position, m_camera.transform.position);
+			m_detector.SetValue(distance);
+			m_distanceText.text = distance.ToString("N");
+		}
+	}
+}
