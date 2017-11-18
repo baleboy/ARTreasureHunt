7961d1efdf717d7790b340951fa4f37286bb4f88
Francesco Balestrieri <bale@balenet.com>
2017-11-18 08:01:38 +0200

    Refactor: move settings logic to Settings Panel controller

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -23,29 +23,15 @@ namespace UnityEngine.XR.iOS {
 		private State m_state = State.Start;
 		private ARTrackingState m_tracking_state = ARTrackingState.ARTrackingStateNotAvailable;
 
-		const string GAME_AREA_MAX_PREF = "GameAreaSizeMax";
 
-		float[] m_maxScale = {5f, 15f, 50f};
 
 
 		void Start () {
-			int scaleOption = PlayerPrefs.GetInt(GAME_AREA_MAX_PREF);
-			m_AreaDropdown.value = scaleOption;
-			m_detector.SetScale(0.2f, m_maxScale[scaleOption]);
+
 			Screen.sleepTimeout = SleepTimeout.NeverSleep;
 		}
 
-		public void OnSettingsButton() {
-			m_MainMenuPanel.SetActive(false);
-			m_SettingsPanel.SetActive(true);
-		}
 
-		public void SetGameAreaSize(int s) {
-			Debug.Log("Max scale option value = " + s.ToString());
-			m_detector.SetMaxScale(m_maxScale[s]);
-			PlayerPrefs.SetInt(GAME_AREA_MAX_PREF, s);
-			Debug.Log("Max scale = " + m_maxScale[s].ToString());
-		}
 
 		// Update is called once per frame
 		void Update () {
---
+++ Assets/Scripts/SettingsPanelController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsPanelController : MonoBehaviour {
+
+	public DetectorController m_Detector;
+	public Dropdown m_AreaDropdown;
+
+	const string GAME_AREA_MAX_PREF = "GameAreaSizeMax";
+	float[] m_maxScale = {5f, 15f, 50f};
+
+	void Start () {
+		int scaleOption = PlayerPrefs.GetInt(GAME_AREA_MAX_PREF);
+		m_AreaDropdown.value = scaleOption;
+		m_Detector.SetScale(0.2f, m_maxScale[scaleOption]);
+	}
+
+	public void SetGameAreaSize(int s) {
+		Debug.Log("Max scale option value = " + s.ToString());
+		m_Detector.SetMaxScale(m_maxScale[s]);
+		PlayerPrefs.SetInt(GAME_AREA_MAX_PREF, s);
+		Debug.Log("Max scale = " + m_maxScale[s].ToString());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
