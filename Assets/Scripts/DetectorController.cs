ff7403fc256ab65782440a862b2f35b20fac21ec
Francesco Balestrieri <bale@balenet.com>
2017-11-19 12:50:18 +0200

    Show victory screen when the treasure is close enough

--- Assets/Scripts/DetectorController.cs
+++ Assets/Scripts/DetectorController.cs
@@ -9,8 +9,13 @@ public class DetectorController : MonoBehaviour {
 	public GameObject m_camera;
 	public GameObject m_marker;
 	public Text m_distanceText;
+	public GameObject m_DetectorPanel;
+	public GameObject m_VictoryPanel;
 
-	float m_min = 0.4f;
+
+	const float MIN_DISTANCE = 0.2f;
+
+	float m_min = MIN_DISTANCE;
 	float m_max = 5;
 	float m_intensity = 0.5f;
 	float m_maxBeepInterval = 3f;
@@ -54,12 +59,23 @@ public class DetectorController : MonoBehaviour {
 		Screen.sleepTimeout = SleepTimeout.SystemSetting;
 	}
 
+	void ShowVictory() {
+		m_DetectorPanel.SetActive(false);
+		m_VictoryPanel.SetActive(true);
+	}
+
 	// Update is called once per frame
 	void Update () {
 
 		float distance = Vector3.Distance(m_marker.transform.position, m_camera.transform.position);
 		SetValue(distance);
 		m_distanceText.text = distance.ToString("N");
+
+		if (distance < MIN_DISTANCE) {
+			// found the treasure
+			ShowVictory();
+			return;
+		}
 
 		m_elapsedTime += Time.deltaTime;
 		if (m_elapsedTime >= m_beepInterval) {
